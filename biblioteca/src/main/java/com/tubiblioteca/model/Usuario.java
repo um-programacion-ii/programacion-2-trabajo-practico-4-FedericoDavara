bae38f4f6790a594e62f85ff137045fd5cfb9ae4
FedericoDavara <f.davara@alumno.um.edu.ar>
2025-05-14 02:57:07 -0300

    Creadas las clases modelo

---
+++ biblioteca/src/main/java/com/tubiblioteca/model/Usuario.java
@@ -0,0 +1,16 @@
+package com.tubiblioteca.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+    private Boolean estado;
+}
+
